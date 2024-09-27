880c500a8e2e466318293b42f417af40483368f9
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-27 18:10:57 +0900

    break／continueのサンプル追加

--- for_sample.js
+++ for_sample.js
@@ -5,6 +5,22 @@ ForSample = {
             str = str + i
         }
         return str
+    },
+    forBreak() {
+        let str = ""
+        for (let i = 0; i < 4; i++) {
+            if (i == 3) break
+            str = str + i
+        }
+        return str
+    },
+    forContinue() {
+        let str = ""
+        for (let i = 0; i < 4; i++) {
+            if (i % 2 == 0) continue
+            str = str + i
+        }
+        return str
     }
 }
 module.exports = ForSample
--- for_sample.test.js
+++ for_sample.test.js
@@ -3,3 +3,11 @@ const ForSample = require('./for_sample.js')
 test('for test', () => {
   expect(ForSample.for()).toBe("0123")
 });
+
+test('forBreak test', () => {
+  expect(ForSample.forBreak()).toBe("012")
+});
+
+test('forContinue test', () => {
+  expect(ForSample.forContinue()).toBe("13")
+});
